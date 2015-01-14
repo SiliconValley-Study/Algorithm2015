91fcf7468de6c969f819f9279336f2d7501cf1ef
namoney86 <namoney86@naver.com>
2015-01-14 23:19:50 +0900

    파일 변경

--- src/sv/algo/ch01/HelloWorld.java
+++ src/sv/algo/ch01/HelloWorld.java
@@ -3,7 +3,7 @@ package sv.algo.ch01;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to SV!");
+		System.out.println("Welcome to SV! 알고리즘 화이팅!");
 	}
 
 }
